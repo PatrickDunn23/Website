6027e8a000f32d9d0cc4509fe6dad84f7c42192f
PatrickDunn23 <142137592+PatrickDunn23@users.noreply.github.com>
2024-02-10 20:39:45 +1000

    working nav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+/* Menu................. */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+
+
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -35,17 +53,3 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
-/* Menu................. */
-$(".searchButton").on("click", function(){
-  $(".searchButton").toggleClass("active");
-  if ($(".searchButton").hasClass("active")) {
-    $(".searchBar").css("height", "100vh");
-    $("#searchForm").css("opacity","1");
-    $(".searchIcon").removeClass("fa-search").addClass("fa-times");
-  }
-  else {
-    $(".searchBar").css('height', "0vh");
-    $("#searchForm").css("opacity","0");
-    $(".searchIcon").removeClass("fa-times").addClass("fa-search");
-  }
-});
